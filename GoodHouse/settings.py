c9223798f62ec4581d69cfb3c4542c40464ef420
broholens <broholens@github.com>
2017-12-06 00:22:45 +0800

    set pipeline and middleware

--- GoodHouse/settings.py
+++ GoodHouse/settings.py
@@ -19,7 +19,7 @@ NEWSPIDER_MODULE = 'GoodHouse.spiders'
 #USER_AGENT = 'GoodHouse (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -52,9 +52,11 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'GoodHouse.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    # 'GoodHouse.middlewares.MyCustomDownloaderMiddleware': 543,
+    'scrapy.downloadermiddleware.useragent.UserAgentMiddleware': None,
+    'GoodHouse.middlewares.ua.RandomUserAgent': 400
+}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
@@ -64,9 +66,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'GoodHouse.pipelines.GoodhousePipeline': 300,
-#}
+ITEM_PIPELINES = {
+    # 'GoodHouse.pipelines.GoodhousePipeline': 300,
+    'GoodHouse.pipelines.mongo.MongoPipeline': 300
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See http://doc.scrapy.org/en/latest/topics/autothrottle.html
@@ -88,3 +91,6 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+MONGO_URI = 'mongodb://localhost:27017'
+MONGO_DATABASE = 'houses'
