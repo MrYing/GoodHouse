fc415beb64fb82400a9bfa76a4ff902879c38198
broholens <broholens@github.com>
2017-12-06 23:03:29 +0800

    update function

--- GoodHouse/utils/f.py
+++ GoodHouse/utils/f.py
@@ -10,14 +10,15 @@ def find(item, xpath, first=True):
     if first is True:
         result = results.extract_first().replace('\n', ' ').split(' ')
         return ''.join(result).strip()
-    return [result.strip() for result in results.extract()]
+    return [''.join(result.replace('\n', ' ').split(' ')).strip()
+            for result in results.extract()]
 
 
 def house_type_split(seq):
     results = []
     for i in seq:
         try:
-            results.append(i.split(' ')[0])
+            results.append(i.split('(')[0].strip())
         except:
             results.append(i)
     return results
