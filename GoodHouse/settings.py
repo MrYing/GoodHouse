7fcba4b1ac65d7e821604e68b1104ffe38515de6
broholens <broholens@github.com>
2017-12-08 17:22:52 +0800

    add new apikey

--- GoodHouse/settings.py
+++ GoodHouse/settings.py
@@ -27,13 +27,13 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See http://scrapy.readthedocs.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 0
+# DOWNLOAD_DELAY = 0
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -46,16 +46,18 @@ DOWNLOAD_DELAY = 0
 
 # Enable or disable spider middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'GoodHouse.middlewares.GoodhouseSpiderMiddleware': 543,
-#}
+# SPIDER_MIDDLEWARES = {
+#     # 'GoodHouse.middlewares.GoodhouseSpiderMiddleware': 543,
+#     # 'scrapy.spidermiddlewares.depth.DepthMiddleware': None
+# }
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 DOWNLOADER_MIDDLEWARES = {
     # 'GoodHouse.middlewares.MyCustomDownloaderMiddleware': 543,
     'scrapy.downloadermiddleware.useragent.UserAgentMiddleware': None,
-    'GoodHouse.middlewares.ua.RandomUserAgent': 400
+    'GoodHouse.middlewares.ua.RandomUserAgent': 1,
+    # 'scrapy.downloadermiddlewares.redirect.RedirectMiddleware': None
 }
 
 # Enable or disable extensions
@@ -68,7 +70,8 @@ DOWNLOADER_MIDDLEWARES = {
 # See http://scrapy.readthedocs.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
     # 'GoodHouse.pipelines.GoodhousePipeline': 300,
-    'GoodHouse.pipelines.mongo.MongoPipeline': 300
+    # 'GoodHouse.pipelines.amap.AMap': 301,
+    'GoodHouse.pipelines.mongo.MongoPipeline': 800
 }
 
 # Enable and configure the AutoThrottle extension (disabled by default)
@@ -94,3 +97,32 @@ ITEM_PIPELINES = {
 
 MONGO_URI = 'mongodb://localhost:27017'
 MONGO_DATABASE = 'houses'
+
+# AMAP_KEYS = [
+#     'c5c96d4b1a01d417d046703ae06d88bd',
+#     '1cc614cd037bd367fe86ffd0d1094540',
+#     'f37aa948833a47f6107650e440aee4ed',
+#     '3415a2784ab651a9ae0135623c84eff3',
+#     '5c52252dd22fdbb9c0f337488d78d3a8',
+#     '637ea37a488d5ada69b9add6b51ade91',
+#     'e1c83a76830ca6be62c67835ac12ad5c',
+#     '1aff8f5ae130e3703d4ba89f46c31df8',
+#     '1f6d0d8ea68c21088beec7e0ca5e7b1e',
+#     'd962e50247065e81077eb144175d5adc'
+# ]
+
+AMAP_KEYS = [
+    '0cf95ad6305d15b44bbd864ffa3c824d',
+    '4eb65384161342b28599bf994c1f2450',
+    'efcbbb3d1a384423b547d6725699d1c4',
+    '5006648b93f630a281f29e9eeaac6ab8',
+    '64e49c52cf205e03b6274ac3b4f66acd',
+    '0652bb3d94666a2b59223d91539e2786',
+    'ba6331d8131baec23f5b0ac4062e8325',
+    '3c6d6676b2c1645d430cba4b8a4db22c',
+    'fc8b23e349a99289bd87d4395cf87252',
+    '4dd66beef79fb206cea77d1a78ff1b7e'
+]
+
+
+LOG_LEVEL = 'INFO'
