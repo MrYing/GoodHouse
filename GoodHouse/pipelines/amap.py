02606877772bcc2059877c595805a5d4bb660219
broholens <broholens@github.com>
2017-12-08 17:16:54 +0800

    stop use this

--- GoodHouse/pipelines/amap.py
+++ GoodHouse/pipelines/amap.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from scrapy.exceptions import DropItem
 
 from GoodHouse.settings import MONGO_URI, MONGO_DATABASE
 from GoodHouse.utils.lbs import LBS
@@ -15,4 +16,13 @@ class AMap(object):
         self.client.close()
 
     def process_item(self, item, spider):
+        # if not item:
+        #     raise DropItem('item Dropped!')
+        if item.get('tag'):
+            return item
+        if 'room' in item['table']:
+            return item
+        if item.get('location'):
+            return item
+
         return self.lbs.get_poi(item)
