6af30b9d9da21f064f5922de039bd868ffdc0dd3
broholens <broholens@github.com>
2017-12-09 01:24:55 +0800

    add souhujiaodian crawler

--- GoodHouse/entrypoint.py
+++ GoodHouse/entrypoint.py
@@ -2,9 +2,11 @@ from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 from GoodHouse.spiders.anjuke import Anjuke
+from GoodHouse.spiders.souhujiaodian import Souhujiaodian
 
 
 process = CrawlerProcess(get_project_settings())
-process.crawl(Anjuke)
+# process.crawl(Anjuke)
+process.crawl(Souhujiaodian)
 process.start()
 
