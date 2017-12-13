c20ec79bd7bee9df1f08f2ab170cf0b00124da3d
broholens <broholens@github.com>
2017-12-13 23:15:40 +0800

    修改xpath

--- GoodHouse/xpath/soufang.py
+++ GoodHouse/xpath/soufang.py
@@ -1,4 +1,4 @@
-PAGE_COUNT = '//div[@class="otherpage"]/span/text()'
+PAGE_COUNT = '//div[@class="otherpage"]/span[2]/text()'
 HOUSE_LINK = '//div[@class="nlc_img"]/a/@href'
 
 NAME = '//a[@class="ts_linear"]/text()'
@@ -6,8 +6,8 @@ ALIAS = '//span[@class="h1_label"]/text()'
 LABELS = '//a[@class="tal_li"]/text()'
 INFO = '//div[@class="main-left"]//li'
 PRICE = '//div[@class="main-info-price"]/em/text()'
-LICENSE = '//div[@class="main-left"]/div[2]//div[@class="table-all"]/tr[position()>1]'
-PRICE_INFO = '//div[@class="main-left"]/div[5]//div[@class="table-all"]/tr[position()>1]'
+HISTORY = '//div[@class="main-left"]//div[@class="table-all"]'
+# PRICE_INFO = '//div[@class="main-left"]/div[5]//div[@class="table-all"]//tr[position()>1]'
 DESCRIPTION = '//p[@class="intro"]/text()'
 
 PICS = '//dl[@id="suofang"]//a'
