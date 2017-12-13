856c915e2958fcfdd661231c2ad111eb9db8dd91
broholens <broholens@github.com>
2017-12-13 16:27:00 +0800

    add soufang

---
+++ GoodHouse/xpath/soufang.py
@@ -0,0 +1,13 @@
+PAGE_COUNT = '//div[@class="otherpage"]/span/text()'
+HOUSE_LINK = '//div[@class="nlc_img"]/a/@href'
+
+NAME = '//a[@class="ts_linear"]/text()'
+ALIAS = '//span[@class="h1_label"]/text()'
+LABELS = '//a[@class="tal_li"]/text()'
+INFO = '//div[@class="main-left"]//li'
+PRICE = '//div[@class="main-info-price"]/em/text()'
+LICENSE = '//div[@class="main-left"]/div[2]//div[@class="table-all"]/tr[position()>1]'
+PRICE_INFO = '//div[@class="main-left"]/div[5]//div[@class="table-all"]/tr[position()>1]'
+DESCRIPTION = '//p[@class="intro"]/text()'
+
+PICS = '//dl[@id="suofang"]//a'
