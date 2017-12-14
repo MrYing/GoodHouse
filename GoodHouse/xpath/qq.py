50fec1849a45edfbdd26b83403f5f0b97f0b3bb8
broholens <broholens@github.com>
2017-12-14 16:07:07 +0800

    info finished

---
+++ GoodHouse/xpath/qq.py
@@ -0,0 +1,7 @@
+IDS = '//div[@id="search_result_list"]/div'
+NEXT_PAGE = '//div[@id="search_result_page"]/a[last()]'
+
+NAME = '//div[contains(@class, "name")]/h2[@class="yh"]/text()'
+DESCRIPTION = '//i[@id="housedetailmore"]/text()'
+BASIC = '//div[contains(@class, "mt bor")]/div[@class="bd"]/ul/li'
+JTPT = '//div[contains(@class, "jtpt")]/div[@class="bd"]/div/div[2]/text()'
