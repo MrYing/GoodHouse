a04257941a0477cb9d0acb5671c4a2db2ec659d4
broholens <broholens@github.com>
2018-04-10 18:39:00 +0800

    except qq

--- GoodHouse/entrypoint.py
+++ GoodHouse/entrypoint.py
@@ -3,10 +3,11 @@ from scrapy.utils.project import get_project_settings
 
 from GoodHouse.spiders.anjuke import Anjuke
 from GoodHouse.spiders.souhujiaodian import Souhujiaodian
+from GoodHouse.spiders.soufang import Soufang
 
 
 process = CrawlerProcess(get_project_settings())
-# process.crawl(Anjuke)
+process.crawl(Anjuke)
 process.crawl(Souhujiaodian)
+process.crawl(Soufang)
 process.start()
-
