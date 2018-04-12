5cab63497c457860956333a8f12c2a909c939fbc
broholens <broholens@github.com>
2018-04-12 15:37:57 +0800

    use requestium get qq houses id

--- GoodHouse/utils/qq_houses_id.py
+++ GoodHouse/utils/qq_houses_id.py
@@ -6,7 +6,7 @@ from requestium import Session
 
 ptn = re.compile('function_button_(\d+)')
 s = Session(
-    webdriver_path='../chromedriver',
+    webdriver_path='chromedriver',
     browser='chrome',
     webdriver_options={'arguments': ['headless']}
 )
@@ -16,10 +16,11 @@ def qq_houses_id(url):
     results = []
     city = url.split('/')[-1]
     s.driver.get(url)
+    url = 'http://db.house.qq.com/index.php?mod=search&act=newsearch&city={}&showtype=1&page={}'.format
     s.transfer_driver_cookies_to_session()
-    html = s.get(url(1)).text
+    html = s.get(url(city, 1)).text
     results.extend(ptn.findall(html))
     total_count = int(html.split(' = ')[-1].rstrip(';'))
     for page in range(2, int(total_count / 10) + 2):
-        results.extend(ptn.findall(s.get(url(page)).text))
-    return [f'http://db.house.qq.com/{city}_{result}/' for result in results]
+        results.extend(ptn.findall(s.get(url(city, page)).text))
+    return city, results
